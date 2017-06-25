c934a727d1a786acaf253ae49a21a726e1c9e8a7
lcyzgdy <MyGuanDY@outlook.com>
2017-06-25 16:58:51 +0800

    更新

--- AEngine/DescriptorHeap.h
+++ AEngine/DescriptorHeap.h
@@ -8,19 +8,46 @@
 #include<queue>
 #include"DX.h"
 
-class DescriptorAllocator
+namespace RenderCore
 {
-protected:
-	static const uint32_t NumDescriptorPerHeap = 256;
-	static mutex m_allocatorMutex;
-	static vector<ComPtr<ID3D12DescriptorHeap>> m_descriptorHeapPool;
+	namespace Heap
+	{
+		class DescriptorAllocator
+		{
+		protected:
+			const uint32_t NumDescriptorPerHeap = 256;
+			mutex m_allocatorMutex;
+			vector<ComPtr<ID3D12DescriptorHeap>> m_cp_descriptorHeapPool;
 
-public:
-	DescriptorAllocator() = default;
-	~DescriptorAllocator() = default;
+		public:
+			DescriptorAllocator() = default;
+			~DescriptorAllocator() = default;
 
-	D3D12_CPU_DESCRIPTOR_HANDLE Allocate(uint32_t count);
-	void DestoryAll();
-};
+			D3D12_CPU_DESCRIPTOR_HANDLE Allocate(uint32_t count);
+			void DestoryAll();
+		};
 
+
+
+		class DescriptorHandle
+		{
+			D3D12_CPU_DESCRIPTOR_HANDLE m_cpuHandle;
+			D3D12_GPU_DESCRIPTOR_HANDLE m_gpuHandle;
+		public:
+			DescriptorHandle();
+			DescriptorHandle(D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle);
+			DescriptorHandle(D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle, D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle);
+			~DescriptorHandle() = default;
+
+			D3D12_CPU_DESCRIPTOR_HANDLE GetCpuHandle();
+			D3D12_GPU_DESCRIPTOR_HANDLE GetGpuHandle();
+
+			bool IsNull();
+			bool IsShaderVisible() const;
+
+			void operator+=(int offsetScaledByDescriptorSize);
+			DescriptorHandle operator+(int offsetScaledByDescriptorSize);
+		};
+	}
+}
 #endif // !__DESCRIPTORHEAP_H__

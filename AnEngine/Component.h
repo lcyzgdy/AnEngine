0d9a4688816c1e575b3609594a6ef09784fb9947
lcyzgdy <MyGuanDY@outlook.com>
2020-05-30 20:04:37 +0800

    定义 Component和Job

---
+++ AnEngine/Component.h
@@ -0,0 +1,27 @@
+#pragma once
+#pragma once
+#ifndef __COMPONENT_H__
+#define __COMPONENT_H__
+
+#include <mutex>
+
+namespace AnEngine::Game
+{
+	class GameObject;
+
+
+	/*
+	 * Component 用于存放数据，且仅存放数据，可多线程读和单线程写
+	 */
+	class Component
+	{
+		GameObject* m_entity;
+	public:
+		virtual ~Component() = default;
+	};
+}
+#endif // __COMPONENT_H__
+
+/*
+ * 一个Component能且只能属于一个Entity
+ */
--- AnEngine/IParallel.h
+++ AnEngine/IParallel.h
@@ -14,6 +14,10 @@ namespace AnEngine::Game
 		GroupBatch
 	};
 
+	/*
+	 * Job 是纯执行方法，可依赖 Component 中的数据，不可依赖 Behaviour 的数据。
+	 * Job 有两种，这里是可并行执行的 Job，因此不建议此处依赖其他 Entity 的Component，否则性能会大幅下降，而且可能死锁。
+	 */
 	class IParallel
 	{
 	public:

d66c29239d8b640d04ad91801494134cfaa8e237
lcyzgdy <MyGuanDY@outlook.com>
2022-02-05 16:15:23 +0800

    整理cmake

---
+++ Source/AnEngine.Interface/Graphics/ICommandList.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __ICOMMANDLIST_HPP__
+#define __ICOMMANDLIST_HPP__
+
+namespace AnEngine::Rendering
+{
+	class ICommandList
+	{
+	public:
+		void DrawMesh();
+	};
+}
+
+#endif // !__ICOMMANDLIST_HPP__

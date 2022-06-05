70847dfb47b1e8503c0cb45ebf58b1c83547e11f
lcyzgdy <MyGuanDY@outlook.com>
2022-06-05 22:15:43 +0800

    DAG相关

---
+++ Source/AnEngine/Common/Graph/DAGGraph.hpp
@@ -0,0 +1,22 @@
+﻿#pragma once
+#ifndef __ANENGINE__DAGGRAPH_HPP__
+#define __ANENGINE__DAGGRAPH_HPP__
+
+#include "../onwind.h"
+
+#include <vector>
+
+#include "GraphVertex.hpp"
+
+namespace AnEngine::Library
+{
+	/// <summary>
+	/// Directed acycline graph, 有向无环图
+	/// </summary>
+	template<typename _DataType, typename _WeightType = float>
+	class DLL_API DAGGraph
+	{
+	};
+}
+
+#endif // !__ANENGINE__DAGGRAPH_HPP__
---
+++ Source/AnEngine/Common/Graph/GraphVertex.hpp
@@ -0,0 +1,37 @@
+﻿#pragma once
+#ifndef __ANENGINE__GRAPHVERTEX_HPP__
+#define __ANENGINE__GRAPHVERTEX_HPP__
+
+#include "../../onwind.h"
+
+namespace AnEngine::Library
+{
+	template<typename T>
+	struct DLL_API GraphVertex
+	{
+		T data;
+	};
+
+	template<typename T>
+	using GraphNode = GraphVertex<T>;
+
+	template<typename _NodeDataType, typename _WeightType>
+	struct DLL_API GraphEdge
+	{
+		GraphVertex<_NodeDataType>& v1;
+		GraphVertex<_NodeDataType>& v2;
+		_WeightType weight;
+
+		GraphEdge() = delete;
+
+		GraphEdge(GraphVertex<_NodeDataType>& _v1, GraphVertex<_NodeDataType>& _v2, const _WeightType& _weight) : v1(_v1), v2(_v2), weight(_weight)
+		{
+		}
+
+		GraphEdge(GraphVertex<_NodeDataType>&& _v1, GraphVertex<_NodeDataType>&& _v2, const _WeightType& _weight) : GraphEdge(std::forward(_v1), std::forward(_v2), weight(_weight))
+		{
+		}
+	};
+}
+
+#endif // !__ANENGINE__GRAPHVERTEX_HPP__
